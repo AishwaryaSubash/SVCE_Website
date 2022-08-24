c01671b082799f23b541908dda4058859a552921
KE2002 <44949462+KE2002@users.noreply.github.com>
2022-08-24 19:55:38 +0530

    okok

--- pages/Academics/index.tsx
+++ pages/Academics/index.tsx
@@ -1,7 +1,32 @@
 import React from "react";
-
+import SideBar from "../../components/SideBar";
+import styles from "./Academics.module.scss";
+import ScienceIcon from "@mui/icons-material/Science";
 const Academics = () => {
-  return <div>hi</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.sideBar}>
+        <SideBar />
+      </div>
+      <div className={styles.emptyForAReason}></div>
+      <div className={styles.departmentsC}>
+        <div className={styles.dept}>
+          <div className={styles.icon}>
+            <ScienceIcon />
+          </div>
+          <div className={styles.content}> Information Technology </div>
+        </div>
+        <div className={styles.dept}>Information Technology</div>
+        <div className={styles.dept}>Information Technology</div>
+        <div className={styles.dept}>Information Technology</div>
+        <div className={styles.dept}>Information Technology</div>
+        <div className={styles.dept}>Information Technology</div>
+        <div className={styles.dept}>Information Technology</div>
+        <div className={styles.dept}>Information Technology</div>
+        <div className={styles.dept}>Information Technology</div>
+      </div>
+    </div>
+  );
 };
 
 export default Academics;
