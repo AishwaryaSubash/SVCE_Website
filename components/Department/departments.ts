8eda8b8be6d136628104a83a64ac0e2a816dca6a
Aishwarya <riya129ash@gmail.com>
2022-08-25 22:39:00 +0530

    A7 Holaaaa ppl

---
+++ components/Department/departments.ts
@@ -0,0 +1,180 @@
+export interface Dept {
+  id: number;
+  name: string;
+  mission: string[];
+  vision: string;
+  content: string;
+}
+
+export const Department: Dept[] = [
+  {
+    id: 1,
+    name: "Department of Applied Chemistry",
+    mission: [],
+    vision: "",
+    content:
+      "The Department of Applied Chemistry started functioning from the day of inception of the College. Dr. T.K. Thirumalpad was the Professor and Head of the department at Nazarathpet campus. The Department has qualified and well experienced faculty and staff. The department is handling Chemistry courses for all the first year under graduate Enggineering programme. The department is well equipped with excellent lab facilities to catter the requirments of first year engineering and second year Chemical enginnering programme.",
+  },
+  {
+    id: 2,
+    name: "Department of Applied Mathematics",
+    mission: [],
+    vision: "",
+    content:
+      "The department, among the oldest, was started in 1985 when the college came into existence headed by Prof. S D Nigam, a well known mathematician. He laid the foundation for teaching mathematics to engineering students and set a trend which continues till today. The department handles mathematical courses for all the branches of B.E., B.Tech., M.E.,and M.Tech.",
+  },
+  {
+    id: 3,
+    name: "Department of Applied Physics",
+    mission: [],
+    vision: "",
+    content:
+      "The Department of Physics, established by late professor Raja Raja Verma in 1985 at Nazarathpet, caters to the basic foundation course, Engineering Physics, a base line for all Engineering stream. The Department has well-established laboratories to accommodate sixty students at a time. The Department has well qualified and dedicated Faculty members with specialization in various disciplines of Physics. It has been recognized as a Research centre by Anna University, Chennai, for conducting Ph.D. Programmes.",
+  },
+
+  {
+    id: 4,
+    name: "Department of Automobile Engineering",
+    mission: [
+      "Vision achieved by",
+      "Providing the state-of-the-art laboratory facilities to train the learners to cater to the changing needs of the industries and the society.",
+      "Providing opportunities for the learners to bring out their total personality and emphasizing ethical values.",
+      "Preparing the learners to meet the growing challenges of the automotive industry and to fulfill the diverse societal needs of our nation.",
+      "Providing research and intellectual resources to address contemporary and complex problems of the industries and to advance interdisciplinary research and applications.",
+      "Providing compassionate and diverse environment to encourage the students to become innovators and job providers.",
+    ],
+    vision:
+      "To be a forerunner in producing Competent and Responsible Engineers in the field of Automobile and Associated areas to meet the ever-changing needs of the Global Automotive Industry and the Society.",
+    content:
+      "The Department of Automobile Engineering was started in the year 1999 with sanctioned intake of 30. The intake was increased from 30 to 60 in the year 2009. The department has well-qualified and experienced faculty and staff members of proven ability and administrative skills. It has well equipped state-of-the-art laboratories that cater to the needs of the students.The department is actively engaged in teaching and research in the areas of Automobile Engineering and related areas.The department is accredited for three years in the year of 2018 by National Board of Accreditation.",
+  },
+
+  {
+    id: 5,
+    name: "Department of Biotechnology",
+    mission: [
+      "To progress the department to attain center of excellence in bioinformatics and bioprocess technologies by providing best Undergraduate, Postgraduate, Doctoral programs and R&D activities within a decade.",
+      "To develop special skilled training programs for graduates to meet the personality characters stipulated by the industries within a period of five years.",
+    ],
+    vision:
+      "To produce higher caliber Biotechnologists to attain new heights in bioinformatics and bioprocess technology as per industrial needs and to provide leaders in the field of Biotechnology.",
+    content:
+      "Biotechnology principles such as domestication of animals for food by ancient civilization, bread and pickle making in food processing industry, wine and beer making in alcoholic beverage industry, selective breeding of crops and crop-rotation in agriculture, and vaccine development and vaccination in health care industry, etc. were adopted by the society and were shown to produce great impact on the living conditions of people in the earlier millennium. Biotechnology had overwhelming growth in the 20th century. ",
+  },
+  {
+    id: 6,
+    name: "Department of Chemical Engineering",
+    mission: [
+      "Department of Chemical Engineering strives to produce graduates who practice Chemical Engineering professionally and ethically in a competitive global environment and contribute to the betterment of society, thereby focusing on the development of engineers to foster innovation through proficiency and effective communication.",
+    ],
+    vision:
+      "To be a leader in Chemical Engineering Education and Research by providing balanced learning and fostering research to enable the learners to meet the challenges of process industries and societal needs.",
+    content:
+      "Department of Chemical Engineering came into existence in the year 1994 with the B.Tech programme in Chemical Engineering. The Department has highly qualified and experienced faculty and staff and has modern infrastructural facilities. The department is well equipped with excellent lab facilities.All the faculty members possess post graduate / Ph.D. qualifications, five faculty members are pursuing their Ph.D.",
+  },
+
+  {
+    id: 7,
+    name: "Department of Civil Engineering",
+    mission: [
+      "Providing state-of-the art resources that contributes to an excellent learning environment.",
+      "Imparting necessary skills, cultivating moral and ethical values.",
+      "Establishing regular interaction and collaboration with industries",
+      "Motivating the students to take up competitive exams and pursue higher education",
+      "Promoting research and development activities in emerging areas of civil engineering and offering services to society and industry through education, research and consultancy activities.",
+    ],
+    vision:
+      "To become a department of excellence in Civil Engineering education and research producing globally competent civil engineers to serve the industry and society.",
+    content:
+      "The Department of Civil Engineering has started functioning from the year 2008, offering B.E. degree programme in Civil Engineering with the sanctioned intake of 30. During the academic year 2013-2014 intake has been increased to 60. At present, the department has eleven faculty members having P.G. specialization in different areas of Civil Engineering such as Structural engineering, Construction planning & management, water resources engineering, Soil mechanics & foundation engineering & Environmental engineering & Management.",
+  },
+  {
+    id: 8,
+    name: "Department of Computer Science and Engineering",
+    mission: [
+      "  Competent and trained faculty enriching the aptitude of the student to generate innovative and effective problem solving skills with professionalism",
+      "Providing strong research and state of the art facility to the students to face dynamic challenges of industry and society",
+      "Educating the students to be successful, ethical and lifelong learners",
+    ],
+    vision:
+      "To excel in providing education and research in the field of computer science and engineering in the cutting edge technology so as to produce competence professional at global level",
+    content:
+      "The Department of Computer Science and Engineering was established in the year 1985. Over the past few years, the department has acquired national and international importance. This has been achieved by the collective and responsive effort of the faculty, the supporting staff and the students. The department is well equipped with excellent computing facilities, and has highly qualified faculty specialized in areas like Cloud Computing, Artificial Intelligence, Data Analytics, Networking, Data Mining etc.",
+  },
+
+  {
+    id: 9,
+    name: "Department of Electrical and Electronics Engineering",
+    mission: [
+      "To provide a strong foundation to the students at undergraduate and post graduate levels which would satisfy the requirements of industry and society and create possibilities for innovation, research and development. The department shall endeavour to bring out the total personality of the students by inculcating in them soft skills and ethical values.",
+    ],
+    vision:
+      "To provide a high standard of education and research in Electrical & Electronics Engineering such that the learners will be transformed in to global contributors.",
+    content:
+      "The Department of Electrical and Electronics Engineering was started in the year 1985 to cater to the needs of the Electrical engineering subjects in Electronics and Communication Engineering, Mechanical Engineering and Computer Science Engineering. Starting with an intake capacity of 60 in 1994 the Department of Electrical and Electronics Engineering is now catering to the growing need of 120 students. The Department has secured permanent affiliation with Anna University and accredited for five years by NBA for the third consecutive time.",
+  },
+  {
+    id: 10,
+    name: "Department of Electronics and Communication Engineering",
+    mission: [
+      "To provide the best pedagogical atmosphere of highest quality through modern infrastructure, latest knowledge and cutting edge skills.",
+      "To fulfill the research interests of faculty and students by promoting and sustaining in house research facilities so as to obtain the reputed publications and patents.",
+      "To educate our students, the ethical and moral values, integrity, leadership and other quality aspects to cater to the growing need for values in the society.",
+    ],
+    vision:
+      "To excel in offering value based quality education in the field of Electronics and Communication Engineering, keeping in pace with the latest developments in technology through exemplary research, to raise the intellectual competence to match global standards and to make significant contributions to the society.",
+    content:
+      "The Department of ECE was established during the academic year 1985-86 with an initial students’ intake of 60. Later, it was increased to 120 during the academic year 2000-01 and subsequently to the sanctioned intake of 180 during the academic year 2012-13. The department is NBA accredited since April 2002 and thereafter continuously we have obtained accreditation for our UG program and it is valid up to 30th June 2022.",
+  },
+
+  {
+    id: 11,
+    name: "Department of Humanities and Social Science",
+    mission: [
+      "  To educate and train learners to use English effectively as a communication medium.",
+      "To think freely, critically, clearly",
+      "To express lucidly and boldly",
+      "To make students interview-ready.",
+    ],
+    vision:
+      "Provide standard English education and training to learners, with special attention to regional medium students and make them aware of societal needs besides self improvement",
+    content:
+      "The Department of English was established in 1985 along with the other departments. It was then headed by Mr K R Lakshmi Narayanan. In its progress, it grew up to be The Department of Humanities and Social Sciences in 1995. The department is presently headed by Prof Dr T Murugavel.",
+  },
+  {
+    id: 12,
+    name: "Department of Information Technology",
+    mission: [
+      "To develop the department into a “Centre of Excellence in Information Technology” offering engineering education to the students at Undergraduate, Postgraduate and Doctoral degree levels, bringing out their total personality, emphasizing ethical values and preparing them to meet the growing challenges in the Information Technology industry as well as research institutes and diverse societal needs of our Nation.",
+    ],
+    vision:
+      "To produce higher calibre technologists and scientists for helping the country to attain new heights in Information Technology research and industrial needs to provide leadership in the field of technical education.",
+    content:
+      "In 1996, Sri Venkateswara College of Engineering pioneered the introduction of the B.Tech degree programme in Information Technology under the affiliation of University of Madras. This is the first of its kind in Indian Universities. The department fulfills the requirements for the award of B.Tech Degree of Anna University. The Venture was initiated under the guidance of our patron Dr.A.C.Muthiah and with the blessings of the Kanchi Paramacharya. When we started, the batch size was 30. This increased to 60 in 1998 and scaled up to 120 by the year 2000. Our growing numbers is indicative of the highly qualified and extremely dedicated teaching faculty of the department who strive for excellence in every sphere of their expertise. It is the constant endeavor of the department to be in touch with changing needs of the IT Industry so as to be responsive in terms of modifications and introduction of new courses to adapt to these technological changes.",
+  },
+
+  {
+    id: 13,
+    name: "Department of Mechanical Engineering",
+    mission: [
+      "  To be renowned for offering Programs in the field of Mechanical Engineering that imparts competent technical knowledge alongwith skill, research & innovation, leadership and life skills needed for the students to contribute and achieve at global level.",
+      "To provide quality education encompassing recent technological developments by continuously upgrading the academic infrastructure thereby enhancing the technical knowledge of students, teachers and supporting staff which facilitates technical assistance to industrial and societal needs.",
+      "To offer need based training to the students in tools relevant to mechanical engineering.",
+    ],
+    vision:
+      "To be a leader in Higher Technical Education and Research by providing the state of the art facilities to transform the learners into global contributors and achievers.",
+    content:
+      "The Department of Mechanical Engineering is one of the three major departments established right from the beginning of the College. The department has well experienced faculty and staff members of proven ability and administrative skills. It has well equipped state-of-the-art laboratories that cater to the needs of the students. The department also takes up industrial consultancy works from various industries.",
+  },
+  {
+    id: 14,
+    name: "Department of Marine Engineering",
+    mission: [
+      "To impart training with technical knowledge and practical skills to enable the Marine Engineering cadets to be competent to carry out the engineering watch keeping duties safely with an aim for making quality marine engineers of International Standards.To enable the cadets to pursue bright career, both ON and 'OFFSHORE'.",
+    ],
+    vision:
+      "To provide high standard of education and quality training fulfilling the level of knowledge and competence specified in the International Conventions on Standards of Training, Certification & Watch keeping (STCW Conventions) as stipulated by International Maritime Organization (IMO).",
+    content:
+      "This college is the first self financing private engineering college in India to get approval by Directorate General of Shipping, Ministry of Shipping, Road Transport and Highways, Government of India to start the four year B.E. Marine Engineering course. The students after graduation, will be able to join as fifth engineers on board ship.",
+  },
+];
