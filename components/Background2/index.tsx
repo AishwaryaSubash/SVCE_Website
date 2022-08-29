4ce33ade1659b205946a339e76fed649ec131413
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2022-08-29 20:54:49 +0530

    Commit 33 suggested by Dev Karthi

--- components/Background2/index.tsx
+++ components/Background2/index.tsx
@@ -15,26 +15,27 @@ const Background2 = () => {
       xmlns="http://www.w3.org/2000/svg"
       version="1.1"
     >
-      {!showCurve && (
-        <g className={styles.g1} transform={`translate(${width}, 0)`}>
-          <motion.path
-            d="M0 486C-62.2 473.8 -124.5 461.5 -181.4 437.9C-238.3 414.3 -289.9 379.3 -336.6 336.6C-383.2 293.8 -424.9 243.4 -449 186C-473.1 128.6 -479.5 64.3 -486 0L0 0Z"
-            fill="#da3f67"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1, duration: 2 }}
-          ></motion.path>
-        </g>
-      )}
-      <g className={styles.g2} transform={`translate(0, ${height})`}>
+      <g className={styles.g1} transform={`translate(${width}, 0)`}>
         <motion.path
-          d="M0 -486C73 -488.3 146 -490.7 186 -449C226 -407.3 233 -321.6 274.4 -274.4C315.7 -227.1 391.4 -218.4 432.4 -179.1C473.4 -139.8 479.7 -69.9 486 0L0 0Z"
+          d="M0 486C-62.2 473.8 -124.5 461.5 -181.4 437.9C-238.3 414.3 -289.9 379.3 -336.6 336.6C-383.2 293.8 -424.9 243.4 -449 186C-473.1 128.6 -479.5 64.3 -486 0L0 0Z"
           fill="#da3f67"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ delay: 1.5, duration: 2 }}
+          transition={{ delay: 1, duration: 2 }}
         ></motion.path>
       </g>
+
+      {!showCurve && (
+        <g className={styles.g2} transform={`translate(0, ${height})`}>
+          <motion.path
+            d="M0 -486C73 -488.3 146 -490.7 186 -449C226 -407.3 233 -321.6 274.4 -274.4C315.7 -227.1 391.4 -218.4 432.4 -179.1C473.4 -139.8 479.7 -69.9 486 0L0 0Z"
+            fill="#da3f67"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5, duration: 2 }}
+          ></motion.path>
+        </g>
+      )}
     </svg>
   );
 };
